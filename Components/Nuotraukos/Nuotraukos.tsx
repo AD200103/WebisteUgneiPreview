a24392125434cbad2a051a6ecc61128fd57ceeb6
UNIVERSE FAN <cheekibreeki456845@gmail.com>
2026-07-09 15:44:57 +0300

    foto carousel done

---
+++ Components/Nuotraukos/Nuotraukos.tsx
@@ -0,0 +1,81 @@
+import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+const Nuotraukos = () => {
+  const [arr, setArr] = useState([]);
+  const stringArr: string[] = [];
+  const [integer, setInteger] = useState(0);
+  const [firstInteger, setFirstInteger] = useState(16);
+  const [lastInteger, setLastInteger] = useState(integer + 1);
+
+  const arrCreation = () => {
+    for (let i = 1; i < 18; i++) {
+      stringArr.push(`foto (${i}).webp`);
+    }
+    setArr(stringArr);
+  };
+
+  const changeIntegerForward = () => {
+    const tick = integer + 1;
+    setFirstInteger(tick - 1);
+    setInteger(tick);
+    setLastInteger(tick + 1);
+    if (tick >= arr.length - 1) {
+      setLastInteger(tick - (arr.length - 1));
+    }
+    if (tick >= arr.length) {
+      setInteger(0);
+      setFirstInteger(arr.length - 1);
+      setLastInteger(1);
+    }
+    console.log(tick);
+  };
+  const changeIntegerBackward = () => {
+    const tick = integer - 1;
+    setFirstInteger(tick - 1);
+    setInteger(tick);
+    setLastInteger(tick + 1);
+
+    if (tick < 1) {
+      setFirstInteger(arr.length - 1);
+    }
+    if (tick < 0) {
+      setInteger(arr.length - 1);
+      setFirstInteger(arr.length - 2);
+      setLastInteger(0);
+    }
+  };
+
+  useEffect(() => {
+    arrCreation();
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <button
+        onClick={() => {
+          changeIntegerBackward();
+        }}
+      >
+        &lt;
+      </button>
+
+      {arr.length > 0 && (
+        <div className={styles.imgContainer}>
+          <img src={`pics/${arr[firstInteger]}`} />
+          <img className={styles.middlePic} src={`pics/${arr[integer]}`} />
+          <img src={`pics/${arr[lastInteger]}`} />
+        </div>
+      )}
+
+      <button
+        onClick={() => {
+          changeIntegerForward();
+        }}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+};
+export default Nuotraukos;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Virselis from "../Components/Virselis/Virselis";
 import Apie from "../Components/Apie/Apie";
 import Servisas from "../Components/Servisas/Servisas";
+import Nuotraukos from "../Components/Nuotraukos/Nuotraukos";
 const Main = () => {
   return (
     <>
       <Virselis />
       <Apie />
       <Servisas />
+      <Nuotraukos />
     </>
   );
 };
