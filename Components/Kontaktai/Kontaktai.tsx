b6c6a35ecb1bfeec0383a39274a70d522c7dfc33
UNIVERSE FAN <cheekibreeki456845@gmail.com>
2026-07-27 21:01:46 +0300

    instagram added

--- Components/Kontaktai/Kontaktai.tsx
+++ Components/Kontaktai/Kontaktai.tsx
@@ -38,10 +38,16 @@ const Kontaktai = () => {
           <p>+37060802009</p>
           <img className={styles.copyIm} src={"contactsPics/copy.svg"} />
         </div>
-        <div className={styles.secBox}>
-          <img src={"contactsPics/phone.svg"} />
-          <p>INSTAGRAM</p>
-        </div>
+        <a
+          href={
+            "https://www.instagram.com/atmosfera_renginiai?igsh=MWhjazIxNnJyczZwdA=="
+          }
+        >
+          <div className={styles.secBox}>
+            <img src={"contactsPics/instagram.svg"} />
+            <p>INSTAGRAM</p>
+          </div>
+        </a>
       </div>
       <div className={`${styles.coppiedBox} ${visible && styles.ative}`}>
         <img
